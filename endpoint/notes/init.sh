4439c5e008143ee8d338ddba5fe8a0d025a3d50a
rsethur <rsethur@gmail.com>
2021-06-30 12:59:15 -0700

    updates

--- endpoint/notes/init.sh
+++ endpoint/notes/init.sh
@@ -4,7 +4,7 @@ export ENDPOINT_NAME='my-endpt1'
 # endpoint file
 export ENDPOINT_FILE=endpoint/endpoint.yml
 # name of new deployment - can be runid etc
-export RC_DEPLOYMENT='dv1'
+export RC_DEPLOYMENT='dv2'
 # deployment file
 export DEPLOYMENT_FILE=endpoint/deployment.yml
 # new model name & version. ideally this can just be a version - and replaced using sed or something (so changes are in git)
